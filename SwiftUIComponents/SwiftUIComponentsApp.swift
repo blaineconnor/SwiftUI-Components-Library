6a5ecb56a4d3e112b38efa018badaaf9cb91cb2a
blaineconnor <fesarman@anadolu.edu.tr>
2024-06-10 10:47:26 +0300

    Initial Commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Fatih Emre Sarman on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
