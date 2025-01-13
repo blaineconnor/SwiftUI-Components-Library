3762d7763c848a50e0b96cf41d29441026cf1a99
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-13 15:58:10 +0300

    Update Spiral.swift

--- SwiftUIComponents/Components/Backgrounds/Spiral.swift
+++ SwiftUIComponents/Components/Backgrounds/Spiral.swift
@@ -12,18 +12,18 @@ struct Spiral: View {
     
     var body: some View {
         TimelineView(.animation) { context in
-            let time = context.date.timeIntervalSince(start) / 12
-            let rotation = 0.8 + 0.2 * abs((cos(.pi * time) + .pi) / .pi)
+            let time = context.date.timeIntervalSince(start) / 120
+            let rotation = 0.8 + 0.2 * abs((cos(.pi * time) + 1.0) / 2.0)
             Canvas { context, size in
                 let radius = 2.0
                 let center = CGPoint(x: size.width / 2 - radius, y: size.height / 2 - radius)
                 let pointSize = CGSize(width: 2 * radius, height: 2 * radius)
                 for i in 0..<3000 {
                     let j = Double(i) * rotation
-                    let p = CGPoint.spiral(at: .radians(j)) * 2
+                    let p = CGPoint.spiral(at: .radians(j)) / 2
                     let path = Circle().path(in: CGRect(origin: center + p, size: pointSize))
-                    let hue = j.truncatingRemainder(dividingBy: 255) / 255
-                    context.fill(path, with: .color(Color(hue: hue, saturation: 1.0, brightness: 1.0)))
+                    let gray = j.truncatingRemainder(dividingBy: 255) / 255
+                    context.fill(path, with: .color(Color(white: gray)))
                 }
             }
         }
@@ -42,8 +42,22 @@ private extension CGPoint {
         self.init(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
     
-    static func *(_ lhs: Self, _ rhs: Double) -> Self {
-        self.init(x: lhs.x * rhs, y: lhs.y * rhs)
+    static func -(_ lhs: Self, _ rhs: Self) -> Self {
+        self.init(x: lhs.x * rhs.x, y: lhs.y * rhs.y)
+    }
+    
+    static func *(_ lhs: Double, _ rhs: Self) -> Self {
+        self.init(x: lhs * rhs.x, y: lhs * rhs.y)
+    }
+    
+    static func /(_ lhs: Self, _ rhs: Double) -> Self {
+        self.init(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+}
+
+extension Color {
+    init(white: Double) {
+        self.init(white: white, opacity: 1.0)
     }
 }
 
